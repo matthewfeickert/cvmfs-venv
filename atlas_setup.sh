3f8b9653fe975d2dff42eb0a01cef6112e0914c2
Matthew Feickert <matthew.feickert@cern.ch>
2022-02-10 15:10:29 -0600

    Add insertion to atlas_setup.sh

--- atlas_setup.sh
+++ atlas_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Ensure that pip can't install outside a virtual environment
+export PIP_REQUIRE_VIRTUALENV=true
+
 # Check to see if we need to run setupATLAS
 command -v lsetup > /dev/null
 if [ "$?" == "1" ]; then
@@ -18,7 +21,45 @@ _venv_name="${1:-venv}"
 if [ ! -d "${_venv_name}" ]; then
     printf "# Creating new Python virtual environment ${_venv_name}\n"
     python3 -m venv "${_venv_name}"
+
     # Do manipulation of activate script
+    _SET_PYTHONPATH=$(cat <<-EOT
+    # Added by https://github.com/matthewfeickert/cvmfs-venv
+    if [ -n "\${_OLD_VIRTUAL_PYTHONPATH:-}" ] ; then
+        PYTHONPATH="\${_OLD_VIRTUAL_PYTHONPATH:-}"
+        export PYTHONPATH
+        unset _OLD_VIRTUAL_PYTHONPATH
+    fi
+EOT
+)
+
+    _RECOVER_OLD_PYTHONPATH=$(cat <<-EOT
+# Added by https://github.com/matthewfeickert/cvmfs-venv
+if [ -n "\${PYTHONPATH:-}" ] ; then
+    _OLD_VIRTUAL_PYTHONPATH="\${PYTHONPATH:-}"
+    unset PYTHONPATH
+    export PYTHONPATH="\$(find \${VIRTUAL_ENV}/lib/ -type d -name site-packages):\${_OLD_VIRTUAL_PYTHONPATH}"
+fi
+EOT
+
+    ed "$(readlink -f ${_venv_name}/bin/activate)" <<EOF
+16i
+$_SET_PYTHONPATH
+.
+wq
+EOF
+)
+
+    ed "$(readlink -f ${_venv_name}/bin/activate)" <<EOF
+60i
+$_RECOVER_OLD_PYTHONPATH
+.
+wq
+EOF
+
+unset _SET_PYTHONPATH
+unset _RECOVER_OLD_PYTHONPATH
+
 fi
 . "${_venv_name}/bin/activate"
 
