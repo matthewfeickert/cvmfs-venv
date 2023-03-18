5cf200cc0522f9fecc24b279a38180a0dda6be94
Matthew Feickert <matthew.feickert@cern.ch>
2023-03-18 02:53:16 -0500

    feat: Rename to 'cvmfs-venv' and make executable (#15)
    * Rename 'atlas_setup.sh' to 'cvmfs-venv.sh'.
    * Make cvmfs-venv.sh executable.
    * Update README to use executable nature and instruct
      users to rename to just 'cvmfs-venv'.
    * Reuse value of 'readlink -f <path to venv>' in later manipulations.

--- cvmfs-venv.sh
+++ cvmfs-venv.sh
@@ -32,6 +32,7 @@ _venv_name="${1:-venv}"
 if [ ! -d "${_venv_name}" ]; then
     printf "# Creating new Python virtual environment '%s'\n" "${_venv_name}"
     python3 -m venv "${_venv_name}"
+    _venv_full_path="$(readlink -f ${_venv_name})"
 
     # When setting up the Python virtual environment shell variables in the
     # main section of the <venv>/bin/activate script, copy the pattern used
@@ -143,8 +144,8 @@ EOT
     # Find the line number of the last line in the PYTHONHOME set if statement
     # block and inject the PYTHONPATH if statement block directly after it
     # (2 lines later).
-    _RECOVER_OLD_PYTHONPATH_LINE="$(($(sed -n '\|unset _OLD_VIRTUAL_PYTHONHOME|=' "${_venv_name}"/bin/activate) + 2))"
-    ed --silent "$(readlink -f "${_venv_name}"/bin/activate)" <<EOF
+    _RECOVER_OLD_PYTHONPATH_LINE="$(($(sed -n '\|unset _OLD_VIRTUAL_PYTHONHOME|=' "${_venv_full_path}"/bin/activate) + 2))"
+    ed --silent "${_venv_full_path}/bin/activate" <<EOF
 ${_RECOVER_OLD_PYTHONPATH_LINE}i
 ${_RECOVER_OLD_PYTHONPATH}
 .
@@ -154,8 +155,8 @@ EOF
     # Find the line number of the last line in deactivate's PYTHONHOME reset
     # if statement block and inject the PYTHONPATH reset if statement block directly
     # after it (2 lines later).
-    _SET_PYTHONPATH_INSERT_LINE="$(($(sed -n '\|    unset PYTHONHOME|=' "${_venv_name}"/bin/activate) + 2))"
-    ed --silent "$(readlink -f "${_venv_name}"/bin/activate)" <<EOF
+    _SET_PYTHONPATH_INSERT_LINE="$(($(sed -n '\|    unset PYTHONHOME|=' "${_venv_full_path}"/bin/activate) + 2))"
+    ed --silent "${_venv_full_path}/bin/activate" <<EOF
 ${_SET_PYTHONPATH_INSERT_LINE}i
 ${_SET_PYTHONPATH}
 .
@@ -164,8 +165,8 @@ EOF
 
     # Find the line number of the deactivate function and inject the cvmfs-venv-rebase directly after it
     # (1 line later).
-    _RUN_REBASE_LINE="$(($(sed -n '\|deactivate ()|=' "${_venv_name}"/bin/activate) + 1))"
-    ed --silent "$(readlink -f "${_venv_name}"/bin/activate)" <<EOF
+    _RUN_REBASE_LINE="$(($(sed -n '\|deactivate ()|=' "${_venv_full_path}"/bin/activate) + 1))"
+    ed --silent "${_venv_full_path}/bin/activate" <<EOF
 ${_RUN_REBASE_LINE}i
 ${_RUN_REBASE}
 .
@@ -174,8 +175,8 @@ EOF
 
     # Find the line number of the unset -f deactivate line in deactivate's destructive unset
     # and inject the cvmfs-venv-rebase reset directly after it (1 line later).
-    _DESCTRUCTIVE_UNSET_LINE="$(($(sed -n '\|unset -f deactivate|=' "${_venv_name}"/bin/activate) + 1))"
-    ed --silent "$(readlink -f "${_venv_name}"/bin/activate)" <<EOF
+    _DESCTRUCTIVE_UNSET_LINE="$(($(sed -n '\|unset -f deactivate|=' "${_venv_full_path}"/bin/activate) + 1))"
+    ed --silent "${_venv_full_path}/bin/activate" <<EOF
 ${_DESCTRUCTIVE_UNSET_LINE}i
 ${_DESCTRUCTIVE_UNSET}
 .
@@ -184,13 +185,15 @@ EOF
 
     # Find the line number of the unset -f cvmfs-venv-rebase line in deactivate's destructive unset
     # and inject the cvmfs-venv-rebase function directly after it (4 lines later).
-    _CVMFS_VENV_REBASE_LINE="$(($(sed -n '\|unset -f cvmfs-venv-rebase|=' "${_venv_name}"/bin/activate) + 4))"
-    ed --silent "$(readlink -f "${_venv_name}"/bin/activate)" <<EOF
+    _CVMFS_VENV_REBASE_LINE="$(($(sed -n '\|unset -f cvmfs-venv-rebase|=' "${_venv_full_path}"/bin/activate) + 4))"
+    ed --silent "${_venv_full_path}/bin/activate" <<EOF
 ${_CVMFS_VENV_REBASE_LINE}i
 ${_CVMFS_VENV_REBASE}
 .
 wq
 EOF
+
+unset _venv_full_path
 
 unset _SET_PYTHONPATH
 unset _RECOVER_OLD_PYTHONPATH
