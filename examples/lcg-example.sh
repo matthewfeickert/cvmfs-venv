99a1950a194615565ed2013e2bd8230218e76581
Matthew Feickert <matthew.feickert@cern.ch>
2023-03-21 17:37:30 -0500

    Add lcg-view example

---
+++ examples/lcg-example.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+mkdir -p ~/.local/bin
+export PATH=~/.local/bin:"${PATH}"  # If ~/.local/bin not on PATH already
+curl -sL https://raw.githubusercontent.com/matthewfeickert/cvmfs-venv/main/cvmfs-venv.sh -o ~/.local/bin/cvmfs-venv
+chmod +x ~/.local/bin/cvmfs-venv
+
+# Guard against this being run in a subshell
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+. "${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh" -3 --quiet  # setuptATLAS
+
+echo "# lsetup 'views LCG_102 x86_64-centos7-gcc11-opt'"
+lsetup 'views LCG_102 x86_64-centos7-gcc11-opt'
+echo "# cvmfs-venv lcg-example"
+cvmfs-venv lcg-example
+echo "# . lcg-example/bin/activate"
+. lcg-example/bin/activate
+
+echo "# python -m pip list --local"
+python -m pip list --local
+echo "# python -m pip show awkward"
+python -m pip show awkward
+echo "# python -m pip install --upgrade awkward"
+python -m pip install --upgrade awkward
+echo "# python -m pip list --local"
+python -m pip list --local
+echo "# python -m pip show awkward"
+python -m pip show awkward
+echo "# python -c 'import awkward; print(awkward.__version__)'"
+python -c 'import awkward; print(awkward.__version__)'
